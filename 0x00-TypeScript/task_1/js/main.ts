69a4411812bf03d55e6a1c6da0390d5f8aabc47b
khaldi505 <ihebkhaldi505@gmail.com>
2021-10-26 06:20:49 -0700

    task 1.2

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -5,4 +5,9 @@ interface Teacher {
     yearsOfExperience?: number,
     location: string;
     additionalProperties: { [prop: string]: any};
-  }
+  };
+
+  interface Directors extends Teacher {
+    test: String;
+    numberOfReports: number;
+   };
