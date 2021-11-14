2ad864261d0b19f3e6bbe5e587f7b69b95610d99
pepsi <pepsi@pop-os.localdomain>
2021-11-14 20:06:59 +0100

    removing task files to re do them again :c

--- 0x00-TypeScript/task_0/js/main.js
+++
@@ -1,7 +0,0 @@
-var student1 = { firstName: "elhaj", LastName: "9asem", age: 69, location: "bo3rada" };
-var student2 = { firstName: "el haj moussa", LastName: "moussa el haj", age: 7, location: "ghardimo" };
-var studentList = [student1, student2];
-var table = document.createElement("table");
-table.insertRow(0).innerHTML = "<td>" + student1.firstName + "</td><td>" + student1.location + "</td>";
-table.insertRow(1).innerHTML = "<td>" + student2.firstName + "</td><td>" + student2.location + "</td>";
-document.body.append(table);
--- 0x00-TypeScript/task_1/js/main.js
+++
@@ -1,6 +0,0 @@
-;
-;
-var printTeacher = function (firstName, lastName) {
-    console.log(firstName[0] + "." + lastName);
-};
-printTeacher("John", "Doe");
