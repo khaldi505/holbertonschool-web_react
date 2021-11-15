82d33e3c66f555f9e8817275f7fdbf72ff7acae5
pepsi <pepsi@pop-os.localdomain>
2021-11-15 22:06:31 +0100

    task one part three ?

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -14,11 +14,6 @@ interface Directors extends Teacher {
     numberOfReports: number
 }
 
-const director1: Directors = {
-    firstName: 'John',
-    lastName: 'Doe',
-    location: 'London',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
-  };
-  console.log(director1);
+
+export default function printTeacher(firstName: string, lastName: string) {return `${firstName[0]}.${lastName}`}
+
