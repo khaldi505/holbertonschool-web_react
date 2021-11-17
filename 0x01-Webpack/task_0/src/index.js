a74f9238be50cbec5674a5ecf72f63bcd870a46c
pepsi <pepsi@pop-os.localdomain>
2021-11-17 14:27:35 +0100

    js updates

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,5 +1,7 @@
-$("body").append( $('<p>Holberton Dashboard</p>') )
+import $ from "jquery";
+
+$(function() {
+    $("body").append( $('<p>Holberton Dashboard</p>') )
 $("body").append('<p>Dashboard data for the students</p>');
 $("body").append('<p>Copyright - Holberton School</p>');
-
-
+});
