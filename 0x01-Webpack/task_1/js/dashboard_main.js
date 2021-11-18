5a8dce1616e4a00b9a1d7cde83eea2f3186b79f8
pepsi <pepsi@pop-os.localdomain>
2021-11-18 15:55:12 +0100

    idk why he asked for 500 as value for wait, but i gotta do it

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -2,21 +2,17 @@ import $ from 'jquery';
 import _ from 'lodash'
 
 let counter = 0;
+
+$("body").append('<p>Holberton Dashboard</p>');
+$("body").append('<p>Dashboard data for the students</p>');
+$("body").append("<button>Click here to get started</button>");
+$("body").append("<p id='count'></p>");
+$("body").append("<p>Copyright - Holberton School</p>");
+
+
 function updateCounter() {
-    counter +=1;
-    return counter;
+    counter += 1;
+    document.getElementById("count").innerHTML = `${counter} clicks on the button`;
 }
 
-
-$(function() {
-    $("body").append('<p>Holberton Dashboard</p>');
-    $("body").append('<p>Dashboard data for the students</p>');
-    $("body").append("<button>Click here to get started</button>");
-    $("body").append("<p id='count'></p>");
-    $("body").append("<p>Copyright - Holberton School</p>");
-    let deb = _.debounce(() => {
-    let counter = updateCounter();
-    document.getElementById("count").innerHTML = `${counter} clicks on the button`;
-    });
-    $("button").on('click', deb)
-});
+$("button").on('click', _.debounce(updateCounter, 500))
