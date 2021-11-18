5f992c6bcf11f92bc9f3332abb66f31bd5e75e5d
pepsi <pepsi@pop-os.localdomain>
2021-11-18 14:54:53 +0100

    task 2

---
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+import _ from 'lodash'
+
+let counter = 0;
+function updateCounter() {
+    return counter + 1
+}
+
+
+$(function() {
+    $("body").append('<p>Holberton Dashboard</p>');
+    $("body").append('<p>Dashboard data for the students</p>');
+    $("body").append("<button>Click here to get started</button>");
+    $("body").append("<p id='count'></p>");
+    $("body").append("<p>Copyright - Holberton School</p>");
+    let deb = _.debounce(() => {
+    counter = updateCounter()
+    document.getElementById("count").innerHTML = `${counter} clicks on the button`
+    })
+    $("button").on('click', deb)
+});
---
+++ 0x01-Webpack/task_1/webpack.config.js
@@ -0,0 +1,12 @@
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+
+module.exports = {
+    entry: './js/dashboard_main.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+      },
+    mode: 'production'
+  };
