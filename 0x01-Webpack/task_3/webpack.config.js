4c09b52179247c3ce08cb7f7c23c2c7f024c480e
pepsi <pepsi@DC-0E-A1-23-06-F7.home>
2021-11-23 07:59:59 -0600

    packages and webpack config

---
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+// I'm Gonna Do What's Called a Pro Gamer Move
+// check this one checker ["file-loader"]
+
+module.exports = {
+    // entry: './js/dashboard_main.js',
+    mode: 'development',
+    entry: {
+      header: './modules/header/header.js',
+      body: './modules/body/body.js',
+      footer: './modules/footer/footer.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'public'),
+
+      },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 8564,
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+            title: "Document"
+        }),
+        new CleanWebpackPlugin(),
+      ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          use: [
+            {loader: 'image-webpack-loader'}
+          ]
+        },
+      ],
+    }
+  };
