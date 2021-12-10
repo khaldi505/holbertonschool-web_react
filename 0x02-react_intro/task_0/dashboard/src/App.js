a4974119bb1bd790a934e3dd0d32c2f95b83403d
pepsi <pepsi@DC-0E-A1-23-06-F7.home>
2021-12-10 05:25:20 -0600

    src/

--- 0x02-react_intro/task_0/dashboard/src/App.js
+++ 0x02-react_intro/task_0/dashboard/src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import logo from './Holberto_logo.jpg'
+import logo from './Holberton_Logo.jpg';
+import './App.css';
 
 function App() {
-    return <div className='container'> 
-    <div className='App-header'><img src={logo} ></img> <h1>School dashboard</h1></div>
-    <div className='App-body'> <p>Login to access the full dashboard</p></div>
-    <div className='App-footer'><p>Copyright 2020 - holberton School</p></div>
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className='App-footer'>
+        <p>Copyright 2020 - holberton School</p>
+      </div>
     </div>
+  );
 }
 
-export default App
+export default App;
