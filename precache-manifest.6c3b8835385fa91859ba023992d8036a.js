16e90342ca0e24a696166f8c24a284c90948a111
pepsi <pepsi@DC-0E-A1-23-06-F7.home>
2021-12-12 08:01:54 -0600

    Updates

---
+++ precache-manifest.6c3b8835385fa91859ba023992d8036a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7a0a9b07df4f86d5bc2101ea1c7935",
+    "url": "/holbertonschool-web_react/index.html"
+  },
+  {
+    "revision": "ee1c4a4e48f56e258194",
+    "url": "/holbertonschool-web_react/static/css/main.6d1ff85f.chunk.css"
+  },
+  {
+    "revision": "e48fa8fe773df933ee29",
+    "url": "/holbertonschool-web_react/static/js/2.2ed1577a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/holbertonschool-web_react/static/js/2.2ed1577a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1c4a4e48f56e258194",
+    "url": "/holbertonschool-web_react/static/js/main.5c6a8fc4.chunk.js"
+  },
+  {
+    "revision": "81d7340ce2d416f86bea",
+    "url": "/holbertonschool-web_react/static/js/runtime-main.ed44bf8c.js"
+  },
+  {
+    "revision": "23d20eb3253de8404d66401eb6db58d5",
+    "url": "/holbertonschool-web_react/static/media/Holberton_Logo.23d20eb3.jpg"
+  }
+]);
